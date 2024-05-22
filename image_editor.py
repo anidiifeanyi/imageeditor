1a1cba8d8cc17561fcc49c1d0b1aa31f8d992a15
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-22 12:30:32 +0100

    save edited images to destination folder

--- image_editor.py
+++ image_editor.py
@@ -15,3 +15,7 @@ for filename in os.listdir(path):
     factor = 1.5
     enhancer = ImageEnhance.Contrast(edit)
     edit = enhancer.enhance(factor)
+    
+    clean_name = os.path.splitext(filename)[0]
+
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
