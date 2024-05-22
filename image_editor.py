4f437a23de2a2f336d50608f2923c348dc0fe3d7
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-22 12:23:59 +0100

    ...

--- image_editor.py
+++ image_editor.py
@@ -7,3 +7,11 @@ pathOut = "./editedImgs" # folder for edited images
 # Loop through and pick files for editing
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
+    
+# sharpening, BW
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+
+    # contrast
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
