cc8b0d958121efc4697deb034d27eeaa2a52b73d
anidiifeanyi <anidiifeanyi@gmail.com>
2024-05-22 12:22:06 +0100

    select files for editing

--- image_editor.py
+++ image_editor.py
@@ -3,3 +3,7 @@ import os
 
 path = "./imgs" # folder for unedited images
 pathOut = "./editedImgs" # folder for edited images
+
+# Loop through and pick files for editing
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
